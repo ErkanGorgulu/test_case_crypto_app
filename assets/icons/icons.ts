b8e4d0f7d362903605ecb4b630f8ac90669c1153
Erkan GÖRGÜLÜ <erkangorgulu@gmail.com>
2023-04-09 19:13:32 +0300

    PRBTEST added required assets

---
+++ assets/icons/icons.ts
@@ -0,0 +1,22 @@
+import Settings from './cog.svg';
+import Search from './magnifier.svg';
+import UpArrow from './up-arrow.svg';
+import DownArrow from './down-arrow.svg';
+import LeftArrow from './left-arrow.svg';
+import FavoriteFilled from './favorite-filled.svg';
+import FavoriteEmpty from './favorite-empty.svg';
+import SquaredPlus from './squared-plus.svg';
+import Recycle from './recycle.svg';
+
+
+export default {
+    Settings,
+    Search,
+    UpArrow,
+    DownArrow,
+    LeftArrow,
+    FavoriteFilled,
+    FavoriteEmpty,
+    SquaredPlus,
+    Recycle,
+};
